088805939b344cf8b37badccb4f53c9a8fd621dc
Reemyos <reem.abramovitz@gmail.com>
2024-11-21 12:40:56 +0200

    Changed npm install method.

--- air_pressure/on_startup.sh
+++ air_pressure/on_startup.sh
@@ -3,8 +3,16 @@
 # Make sure npm is installed and if not install it
 if ! [ -x "$(command -v npm)" ]; then
   echo 'npm is not installed. Installing now...' >&2
-  apt-get update
-  apt-get install -y npm
+  # installs nvm (Node Version Manager)
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+  # download and install Node.js (you may need to restart the terminal)
+  nvm install 22
+  # verifies the right Node.js version is in the environment
+  node -v # should print `v22.11.0`
+  # verifies the right npm version is in the environment
+  npm -v # should print `10.9.0`
+  mkdir ~/.npm-packages
+  npm config set prefix ~/.npm-packages
   echo 'Installed npm' >&2
 else
   echo 'npm is already installed' >&2
