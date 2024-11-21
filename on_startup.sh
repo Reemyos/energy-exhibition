93d0eea1d743c8d1ea615bb897dfa6da2ca84ae7
Reemyos <reem.abramovitz@gmail.com>
2024-11-21 12:49:12 +0200

    The serial_to_websocket will run in the background.

--- on_startup.sh
+++ on_startup.sh
@@ -1,6 +1,7 @@
+# Run the serial_to_websocket/on_startup.sh script in a subshell
+sh ./serial_to_websocket/on_startup.sh &
+
 # Run the air_pressure/on_startup.sh script
 sh ./air_pressure/on_startup.sh
 
-# Run the serial_to_websocket/on_startup.sh script
-sh ./serial_to_websocket/on_startup.sh
 
